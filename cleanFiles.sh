827fa595a5790b51b36b882eceb2b54f4f803b90
Andreas Till <andreas.till@netzint.de>
2020-11-19 09:52:47 +0100

    added fix ipaddress for all container, added cleanup remove dockers

--- cleanFiles.sh
+++ cleanFiles.sh
@@ -2,3 +2,13 @@
 rm -Rf -v ./db01-data/*
 rm -Rf -v ./db02-data/*
 rm -Rf -v ./nextcloud/data/*
+
+docker rm ni-nextcloud_nc-web04_1
+docker rm ni-nextcloud_nc-web01_1
+docker rm ni-nextcloud_nc-web02_1
+docker rm ni-nextcloud_app_1
+docker rm ni-nextcloud_nc-proxy01_1
+docker rm ni-nextcloud_nc-db01_1
+docker rm ni-nextcloud_redis01_1
+docker rm ni-nextcloud_nc-db02_1
+docker rm ni-nextcloud_nc-sqlbroker_1
