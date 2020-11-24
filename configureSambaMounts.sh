412654e743dd2e8380472fed33e9830b032da7ef
Andreas Till <andreas.till@netzint.de>
2020-11-24 20:02:59 +0000

    iupdateded configureSambamounts.sh and externalStorage template

--- configureSambaMounts.sh
+++ configureSambaMounts.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-source samba.env
-
 
 docker cp templates/externalStorage.json nextcloud_docker_cluster_fpm01_1:/
 docker exec --user www-data -it nextcloud_docker_cluster_fpm01_1 /var/www/html/occ files_external:import /externalStorage.json
