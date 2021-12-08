91efd8885757d9f93180467503129748e3508b95
Lukas Spitznagel <lukas.spitznagel@netzint.de>
2021-12-08 11:45:03 +0100

    Add buildscript for docker container

---
+++ nextcloud-fpm/buildVersion.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while read line; do
+  echo "Building nextcloud image with version $line"
+  sed -i "s/FROM .*/FROM nextcloud:$line-fpm/g" Dockerfile
+  docker build -t netzint/nextcloud-fpm:$line .
+  docker push netzint/nextcloud-fpm:$line
+done <versions.txt
