a7e1827900a36a15f1d512900a8709f25edc6ae3
Lukas Spitznagel <lukas.spitznagel@netzint.de>
2022-08-18 09:43:39 +0200

    Fix bug in allowFraming and add versions

--- 82_allowFraming.sh
+++ 82_allowFraming.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source ./db.env
 
-sed -i "s/add_header X-Frame-Options \"SAMEORIGIN\";/#add_header X-Frame-Options \"SAMEORIGIN\";/g" web/web*/nextcloud
-sed -i "/add_header X-Frame-Options \"SAMEORIGIN\";/i \    add_header X-Frame-Options \"ALLOW-FROM $SERVER_URL\";\n    add_header Content-Security-Policy \"frame-ancestors $SERVER_URL\";" web/web*/nextcloud
+sed -i "s/add_header X-Frame-Options \"SAMEORIGIN\";/#add_header X-Frame-Options \"SAMEORIGIN\";/g" web/web*/nextcloud.conf
+sed -i "/add_header X-Frame-Options \"SAMEORIGIN\";/i \    add_header X-Frame-Options \"ALLOW-FROM $SERVER_URL\";\n    add_header Content-Security-Policy \"frame-ancestors $SERVER_URL\";" web/web*/nextcloud.conf
 
 docker exec nextcloud_docker_cluster_fpm01_1 sed -i "/allowedFrameAncestors = \[/a \                '$SERVER_URL'," /var/www/html/lib/public/AppFramework/Http/ContentSecurityPolicy.php
 docker exec nextcloud_docker_cluster_fpm01_1 sed -i "s/\[\]/['$SERVER_URL']/g" /var/www/html/lib/public/AppFramework/Http/ContentSecurityPolicy.php
